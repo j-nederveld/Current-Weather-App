586c0a8bf82fa10c838cac86af3fafc0065db75f
John Nederveld <john.nederveld@gmail.com>
2020-09-20 12:13:22 -0400

    consolidated functionality for the 5 day forecast into a loop. will get the daily high/low temp as well as the icon associated with the specific date

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,11 @@
   // var dateEl = document.querySelector("#showDate");
   var currentDate = moment().format("dddd, MMMM Do" );
   $("#currentDay").text(currentDate);
+  var forecastDay1 = moment().add(1, 'd').format("dddd, MMMM Do" );
+  var forecastDay2 = moment().add(2, 'd').format("dddd, MMMM Do" );
+  var forecastDay3 = moment().add(3, 'd').format("dddd, MMMM Do" );
+  var forecastDay4 = moment().add(4, 'd').format("dddd, MMMM Do" );
+  var forecastDay5 = moment().add(5, 'd').format("dddd, MMMM Do" );
 
 
   //get weather data using openweathermap API 
@@ -18,77 +23,60 @@ $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
+    //grab lat/lon for the next API call
      longitude = (response.coord.lon);
      latitude = (response.coord.lat);
-        
+
+    //inject the current weather data into the page 
   $(".city").text("City: " + response.name);
   $(".wind").text("Wind: " + response.wind.speed + " mph");
   $(".humidity").text("Humidity: " + response.main.humidity + "%");
   $(".temp").text("Temperature: " + Math.floor((response.main.temp - 273.15) * 9/5 + 32));
 
+  //grab the current UV data
   $.ajax({
     url: "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey,
     method: "GET"
   }).then(function(response) {
        
-       $(".uv-index").text("UV Index: " + response.value);
+    $(".uv-index").text("UV Index: " + response.value);
 
+    //sort the UV index and color code the text background accordingly
     if (response.value < 3) {
         $(".uv-index").addClass("mild");
+        $(".uv-index").removeClass("moderate");
+        $(".uv-index").removeClass("severe");
     }
     if (response.value > 3 && response.value < 7) {
-        $(".uv-index").addClass("mild");
+        $(".uv-index").addClass("moderate");
+        $(".uv-index").removeClass("mild");
+        $(".uv-index").removeClass("severe");
     }
     if (response.value > 7) {
         $(".uv-index").addClass("severe");
+        $(".uv-index").removeClass("moderate");
+        $(".uv-index").removeClass("mild");
     }
-    // console.log(response);
-    var avgTempDayOne = [];
-    var avgTempDayTwo = [];
-    var avgTempDayThree = [];
-    var avgTempDayFour = [];
-    var avgTempDayFive = [];
-
+    
+    //make the last API call for our 5 day forecast
     $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey,
+        url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=current,minutely, hourly" + "&appid=" + apiKey,
         method: "GET"
       }).then(function(response) {
-          console.log(response);
-        //loop through each day (day is 0-7, 8-15...to 39)
-        for (i = 0; i < 8; i++) {        
-            avgTempDayOne.push(response.list[i].main.temp);
-        }
-        for (i = 8; i < 16; i++) {        
-            avgTempDayTwo.push(response.list[i].main.temp);
-        }
-        for (i = 16; i < 24; i++) {        
-            avgTempDayThree.push(response.list[i].main.temp);
-        }
-        for (i = 24; i < 32; i++) {        
-            avgTempDayFour.push(response.list[i].main.temp);
-        }
-        for (i = 32; i <= 39; i++) {        
-            avgTempDayFive.push(response.list[i].main.temp);
-        }
-        //get the average temp in K for each day
-        avgTempDayOne = avgTempDayOne.reduce((a, b) => a + b, 0)/8
-        avgTempDayTwo = avgTempDayTwo.reduce((a, b) => a + b, 0)/8
-        avgTempDayThree = avgTempDayThree.reduce((a, b) => a + b, 0)/8
-        avgTempDayFour = avgTempDayFour.reduce((a, b) => a + b, 0)/8
-        avgTempDayFive = avgTempDayFive.reduce((a, b) => a + b, 0)/8
+        //add next 5 dates to the forecast h3
+        $("#day-1").text(forecastDay1);
+        $("#day-2").text(forecastDay2);
+        $("#day-3").text(forecastDay3);
+        $("#day-4").text(forecastDay4);
+        $("#day-5").text(forecastDay5);
 
-        //convert K to Fahrenheit 
-        avgTempDayOne = Math.floor((avgTempDayOne - 273.15) * 9/5 + 32);
-        avgTempDayTwo = Math.floor((avgTempDayTwo - 273.15) * 9/5 + 32);
-        avgTempDayThree = Math.floor((avgTempDayThree - 273.15) * 9/5 + 32);
-        avgTempDayFour = Math.floor((avgTempDayFour - 273.15) * 9/5 + 32);
-        avgTempDayFive = Math.floor((avgTempDayFive - 273.15) * 9/5 + 32);
-
-        console.log("Day 1: " + avgTempDayOne);
-        console.log("Day 2: " + avgTempDayTwo);
-        console.log("Day 3: " + avgTempDayThree);
-        console.log("Day 4: " + avgTempDayFour);
-        console.log("Day 5: " + avgTempDayFive);
+        //grab the high/low temp for the next 5 days as well as the weather icon associated with each day
+        for (i = 1; i < 6; i++) {
+            $(".high-" + i).text("High: " + Math.floor((response.daily[i].temp.max - 273.15) * 9/5 + 32));
+            $(".low-" + i).text("Low: " + Math.floor((response.daily[i].temp.min - 273.15) * 9/5 + 32));
+            $(".icon-" + i).attr("src", "http://openweathermap.org/img/wn/" + response.daily[i].weather[0].icon + ".png");
+            $(".icon-" + i).removeClass("hide");
+        }
               
 });
 });  
